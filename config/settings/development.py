e891fce72ff6cca9776b6cfeb71bd79a88d36361
AbuArwa001 <khalfanathman12@yahoo.com>
2026-07-28 16:13:15 +0300

    chore: update development database configuration to use dj_database_url

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,11 +5,14 @@ from .base import *  # noqa
 
 DEBUG = True
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Allow all origins in development
